619271f0f81451a87a6f8c9a7ede7b5058d435ac
sukawirawan <sukawirawan@proton.me>
2023-02-28 14:13:03 +0800

    commit app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import JSONPretty from 'react-json-pretty';
+import { Button } from '@material-tailwind/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface propsInf {}
+interface stateInf {
+  json: string,
+  jsonPretty: string
 }
 
-export default App;
+export default class App extends Component<propsInf, stateInf> {
+  constructor (props: any) {
+    super (props)
+
+    this.state = {
+      json: '{"hello": "world"}',
+      jsonPretty: '{}'
+    }
+  }
+
+  _transform = (): void => {
+    this.setState({
+      jsonPretty: this.state.json
+    })
+  }
+
+  _handleChange = (e: any): void => {
+    this.setState({
+      json: e.target.value
+    })
+  }
+
+  render () {
+    const {json, jsonPretty} = this.state
+    return (
+      <>
+        <h1 className="text-3xl text-center font-bold pt-8">
+          JSON Formatter
+        </h1>
+        <hr  className='my-4'/>
+        <div className='container mx-auto px-4'>
+
+        <div className='grid grid-cols-1 gap-4'>
+          <div>
+            
+          <label 
+              htmlFor="json" 
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+              JSON
+          </label>
+          <textarea 
+              id="message" 
+              rows={18}
+              className="
+                block p-2.5 w-full text-sm
+                text-gray-700 bg-gray-50 rounded-md border
+                border-gray-300
+              "
+              placeholder="Write your json here..."
+              value={json}
+              onChange={e => this._handleChange(e)}
+          />
+          </div>
+          <div>
+            <Button onClick={() => this._transform()}>Proccess</Button>
+          </div>
+          <div>
+            <JSONPretty 
+              data={jsonPretty}
+            />
+          </div>
+        </div>
+        </div>
+      </>
+    )
+  }
+}
